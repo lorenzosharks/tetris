e70956d81056b98645b2ebcfcae53eb0127572d9
lorenzosharks <karsonx27@lakesideschool.org>
2024-07-30 22:01:31 -0700

    stuff

--- game.py
+++ game.py
@@ -2,9 +2,11 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((400, 720))
 clock = pygame.time.Clock()
 running = True
+
+background = pygame.image.load("assets/background.png")
 
 while running:
     # poll for events
@@ -13,8 +15,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.blit(background, (0,0))
+
+    pygame.draw.rect(screen, "blue", (80, 80, 240, 560))
+
+    pygame.draw.rect(screen, "black", (100, 100, 200, 520))
+
+    for x in range(99, 301, 20):
+        pygame.draw.line(screen, "white", (x,100), (x,619))
+    for y in range(99, 620, 20):
+        pygame.draw.line(screen, "white", (100,y), (299,y))
 
     # RENDER YOUR GAME HERE
 
